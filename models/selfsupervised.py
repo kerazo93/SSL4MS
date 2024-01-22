5a05a96d6f8570e37be88dcc2cfbde095a593537
Kevin Erazo Castillo <kevinerazocastillo@Kevins-MBP-2.localdomain>
2024-01-21 22:56:49 -0800

    built spectrum encoder and decoder modules

--- models/selfsupervised.py
+++ models/selfsupervised.py
@@ -2,13 +2,59 @@ from layers.modules import PositionalEncoding, EncoderLayerGLU, DecoderLayerGLU
 import torch
 from torch import nn
 
-class SpectrumEncoder(nn.Module):
-    def __init__(self):
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, num_heads, ffn_factor, dropout, act):
         super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.num_heads = num_heads
+        self.ffn_factor = ffn_factor
+        self.dropout = dropout
+        self.act = act
+
+        self.cnn = nn.Conv1d(in_channels=self.in_dim, out_channels=self.out_dim, padding='same', stride=1, kernel_size=1)
+        self.enc = EncoderLayerGLU(d_model=self.out_dim, num_heads=self.num_heads, d_ffn=self.ffn_factor*self.out_dim, act=self.act, dropout=self.dropout)
+
+    def forward(self, x, mask):
+        x = torch.permute(x, (0,2,1))
+        x = self.cnn(x)
+        x = torch.permute(x, (0,2,1))
+        x = x * ~mask.unsqueeze(2)
+        x = self.enc(x, mask)
+        return x
 
 
-    def forward(self):
-        pass
+
+class SpectrumEncoder(nn.Module):
+    def __init__(self, num_heads, ffn_factor, dropout, act, hidden_dims):
+        super().__init__()
+        self.num_heads = num_heads
+        self.ffn_factor = ffn_factor
+        self.dropout = dropout
+        self.act = act
+
+        self.hidden_dims = sorted(hidden_dims)
+        self.all_dims = [2] + self.hidden_dims
+
+        self.enc_list = nn.ModuleList()
+
+        for a,b in zip(self.all_dims, self.all_dims[1:]):
+            enc_layer = EncoderBlock(in_dim=a, out_dim=b, num_heads=self.num_heads, ffn_factor=self.ffn_factor, dropout=self.dropout, act=self.act)
+            self.enc_list.append(enc_layer)
+
+
+    def generate_mask(self, x):
+        mask = x.sum(axis=-1)==0
+        return mask
+
+    def forward(self, x):
+        mask = self.generate_mask(x)
+
+        for layer in self.enc_list:
+            x = layer(x, mask)
+
+        return x, mask
 
 
 
@@ -16,9 +62,42 @@ class SpectrumEncoder(nn.Module):
 
 
 class SpectrumDecoder(nn.Module):
+    def __init__(self, num_heads, ffn_factor, dropout, act, hidden_dims):
+        super().__init__()
+        self.num_heads = num_heads
+        self.ffn_factor = ffn_factor
+        self.dropout = dropout
+        self.act = act
+
+        self.hidden_dims = sorted(hidden_dims, reverse=True)
+
+        self.dec_list = nn.ModuleList()
+
+        for a,b in zip(self.hidden_dims, self.hidden_dims[1:]):
+            dec_layer = EncoderBlock(in_dim=a, out_dim=b, num_heads=self.num_heads, ffn_factor=self.ffn_factor, dropout=self.dropout, act=self.act)
+            self.dec_list.append(dec_layer)
+
+        self.final_cnn = nn.Conv1d(in_channels=self.hidden_dims[-1], out_channels=2, padding='same', stride=1, kernel_size=1)
+
+    def forward(self, x, mask):
+        for layer in self.dec_list:
+            x = layer(x, mask)
+
+
+        x = torch.permute(x, (0,2,1))
+        x = nn.ReLU()(self.final_cnn(x))
+        x = torch.permute(x, (0,2,1))
+        x = x * ~mask.unsqueeze(2)
+        return x
+
+
+
+
+class SpectrumSymmetricAE(nn.Module):
     def __init__(self):
         super().__init__()
 
 
-    def forward(self):
+
+    def forward(self, x):
         pass
